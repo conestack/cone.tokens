2866b4486784d546766360f80f35e59775ec62a7
toalba <tb@kleinundpartner.at>
2023-07-06 10:07:23 +0200

    add token tests

--- src/cone/tokens/tests.py
+++ src/cone/tokens/tests.py
@@ -65,6 +65,53 @@ class TestTokens(NodeTestCase):
         session.delete(token)
         self.assertRaises(TokenNotExists,token_api.consume)
 
+    @sql_testing.delete_table_records(TokenRecord)
+    def test_token_add(self):
+        request = self.layer.new_request()
+        session = get_session(request)
+        token_api = Tokens(request,uuid.uuid4())
+        token_api.add(datetime.now()+timedelta(1),-1,0)
+        result = session.query(TokenRecord).one()
+        self.assertEqual(isinstance(result, TokenRecord), True)
+    
+    @sql_testing.delete_table_records(TokenRecord)
+    def test_token_delete(self):
+        request = self.layer.new_request()
+        session = get_session(request)
+        token = TokenRecord()
+        token.uid = uuid.uuid4()
+        token.valid_from = datetime.now()
+        token.valid_to = datetime.now()+timedelta(1)
+        token.lock_time = 0
+        token.usage_count = -1
+        session.add(token)
+        session.commit()
+        token_api = Tokens(request, token.uid)
+        token_api.delete()
+        result = session.query(TokenRecord).all()
+        self.assertEqual(result, [])
+    
+    def test_token_update(self):
+        request = self.layer.new_request()
+        session = get_session(request)
+        token = TokenRecord()
+        token.uid = uuid.uuid4()
+        token.valid_from = datetime.now()
+        token.valid_to = datetime.now()+timedelta(1)
+        token.lock_time = 0
+        token.usage_count = -1
+        session.add(token)
+        session.commit()
+        token_api = Tokens(request, token.uid)
+        token_api.update(datetime(2022,1,1),datetime(2022,1,2),1,120)
+        result = session.query(TokenRecord).one()
+        self.assertEqual(isinstance(result, TokenRecord), True)
+        self.assertEqual(result.valid_from,datetime(2022,1,1))
+        self.assertEqual(result.valid_to,datetime(2022,1,2))
+        self.assertEqual(result.lock_time,120)
+        self.assertEqual(result.usage_count,1)
+
+
 
 
 def run_tests():
