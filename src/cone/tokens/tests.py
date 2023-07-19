f0034d84843087a8cb684e657adfc5079d6755fd
toalba <tb@kleinundpartner.at>
2023-07-19 10:46:44 +0200

    add edit form test

--- src/cone/tokens/tests.py
+++ src/cone/tokens/tests.py
@@ -1,7 +1,7 @@
 from cone.sql import get_session
 from cone.sql import testing as sql_testing
 from cone.sql.testing import SQLLayer
-from cone.tokens.browser.token import TokenAddForm, TokenForm
+from cone.tokens.browser.token import TokenAddForm, TokenEditForm, TokenForm
 from cone.tokens.exceptions import TokenLockTimeViolation
 from cone.tokens.exceptions import TokenNotExists
 from cone.tokens.exceptions import TokenTimeRangeViolation
@@ -93,6 +93,7 @@ class TestTokens(NodeTestCase):
         result = session.query(TokenRecord).all()
         self.assertEqual(result, [])
     
+    @sql_testing.delete_table_records(TokenRecord)
     def test_token_update(self):
         request = self.layer.new_request()
         session = get_session(request)
@@ -113,7 +114,9 @@ class TestTokens(NodeTestCase):
         self.assertEqual(result.lock_time,120)
         self.assertEqual(result.usage_count,1)
 
-    def test_token_form(self):
+
+    @sql_testing.delete_table_records(TokenRecord)
+    def test_token_addform(self):
         request = self.layer.new_request()
         # create processor
         token = TokenNode()
@@ -140,6 +143,65 @@ class TestTokens(NodeTestCase):
     __<class 'yafowil.base.Widget'>: cancel
     __<class 'yafowil.base.Widget'>: came_from
     __<class 'yafowil.base.Widget'>: factory
+        """, form_tile.form.treerepr(prefix='_'))
+        data = form_tile.form.extract(request=request)
+        self.assertEqual(
+            data.fetch('tokenform.valid_from').extracted,
+            ''
+        )
+        self.assertEqual(
+            data.fetch('tokenform.valid_to').extracted,
+            UNSET
+        )
+        self.assertEqual(
+            data.fetch('tokenform.usage_count').extracted,
+            UNSET
+        )
+        self.assertEqual(
+            data.fetch('tokenform.lock_time').extracted,
+            UNSET
+        )
+        # Save processor
+        request.params['tokenform.valid_from'] = datetime(2022,1,1)
+        request.params['tokenform.valid_to'] = datetime(2022,1,2)
+        request.params['tokenform.usage_count'] = -1
+        request.params['tokenform.lock_time'] = 0
+        request.params['action.tokenform.save'] = '1'
+        form_tile(token, request)
+
+        # Check if data is saved
+        self.assertEqual(token.attrs['valid_from'], datetime(2022,1,1))
+        self.assertEqual(token.attrs['valid_to'], datetime(2022,1,2))
+        self.assertEqual(token.attrs['usage_count'], -1)
+        self.assertEqual(token.attrs['lock_time'], 0)
+
+    @sql_testing.delete_table_records(TokenRecord)
+    def test_token_editform(self):
+        request = self.layer.new_request()
+        # create processor
+        token = TokenNode()
+        token.attrs['uid'] = uuid.uuid4()
+        token.attrs['valid_from'] = datetime.now()
+        token.attrs['valid_to'] = datetime.now() + timedelta(1)
+        token.attrs['lock_time'] = 0
+        token.attrs['usage_count'] = -1
+
+        # prepare processor form
+        form_tile = TokenEditForm(None, 'render', 'editform')
+        form_tile.model = token
+        form_tile.request = request
+        form_tile.action_resource = 'tokenform'
+        form_tile.prepare()
+        self.assertEqual(form_tile.form.name, 'tokenform')
+        self.checkOutput("""    
+    <class 'yafowil.base.Widget'>: tokenform
+    __<class 'yafowil.base.Widget'>: valid_from
+    __<class 'yafowil.base.Widget'>: valid_to
+    __<class 'yafowil.base.Widget'>: usage_count
+    __<class 'yafowil.base.Widget'>: lock_time
+    __<class 'yafowil.base.Widget'>: save
+    __<class 'yafowil.base.Widget'>: cancel
+    __<class 'yafowil.base.Widget'>: came_from
         """, form_tile.form.treerepr(prefix='_'))
         data = form_tile.form.extract(request=request)
         self.assertEqual(
